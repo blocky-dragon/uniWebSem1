ceda9f9b3a785913b6f1353c07c544af573dc524
blocky-dragon <andrewdouglas004@gmail.com>
2023-10-31 12:02:41 +0000

    rss changes

--- week5/rssReader.js
+++ week5/rssReader.js
@@ -1,31 +1,74 @@
-
 var myRss = null;
+var rssSource = "https://moxie.foxnews.com/google-publisher/latest.xml";
 
+var currentSource = 0;
+var sources = ["https://moxie.foxnews.com/google-publisher/latest.xml", "https://moxie.foxnews.com/google-publisher/politics.xml"]
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    // BEGIN: abpxx6d04wxr
-    // create a new div element
+    // Create a new div element
+
+    
+
+
     const div = document.createElement('div');
 
-    // set the innerHTML of the div to some basic HTML
+    // Set the innerHTML of the div to some basic HTML
     div.innerHTML = `
             <h1>RSS Parser</h1>
             <p>Current example: Fox news</p>
     `;
 
-    // append the div to the body of the HTML document
+    // Append the div to the body of the HTML document
     document.body.appendChild(div);
-    // END: abpxx6d04wxr
-    flow();
 
+    // Create a new button element
+    const button = document.createElement('button');
+    button.innerHTML = 'Change RSS Source';
+
+    const rssContainer = document.createElement('div');
+    rssContainer.id = 'rssContainer';
+
+
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+        // Change the RSS source
+
+        if(currentSource == 0){
+            currentSource = 1;
+        }
+        else{
+            currentSource = 0;
+        }
+
+
+        rssSource = sources[currentSource];
+
+        div.innerHTML = `
+            <h1>RSS Parser</h1>
+            <p>Current example: ${rssSource}</p>
+        `;
+
+
+        // Call the flow function to update the RSS feed
+        flow();
+    });
+
+    // Append the button to the body of the HTML document
+    document.body.appendChild(button);
+
+    // Append the RSS container to the body of the HTML document
+    document.body.appendChild(rssContainer);
+
+
+    flow();
 });
 
 //get data
 async function getRssData(){
     //get data
     
-    const response = await fetch("https://moxie.foxnews.com/google-publisher/latest.xml");
+    const response = await fetch(rssSource);
     const data = await response.text();
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(data, "text/xml");
@@ -51,6 +94,13 @@ async function flow(){
 
 
 async function generateSite(){
+
+
+
+    const rssContainer = document.getElementById('rssContainer');
+
+    rssContainer.innerHTML = "";
+
     for(var i = 0; i < myRss.length; i++){
         var currentItem = myRss[i];
         var title = currentItem.title;
@@ -65,6 +115,6 @@ async function generateSite(){
             </div>
         </div>
         `;
-        document.body.innerHTML += html;
+        rssContainer.innerHTML += html;
     }
 }
