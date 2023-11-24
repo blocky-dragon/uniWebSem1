3e920cd5cc5111ccc072db85583a597389352344
blocky-dragon <andrewdouglas004@gmail.com>
2023-11-24 09:10:04 +0000

    password example

---
+++ week11/passwordChecker.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('form').addEventListener('submit', function(event) {
+        var password = document.getElementById('pwd').value;
+        if (password.length < 8) {
+            alert('Password must be at least 8 characters long.');
+            event.preventDefault();
+        }
+    });
+});
+
+
+console.log("hello world");
